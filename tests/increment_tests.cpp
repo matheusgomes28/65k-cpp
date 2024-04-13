33465fe13b296df47b0ef3a0e9dc39f3a4566300
matheusgomes28 <matheusgarcia28@gmail.com>
2024-04-13 10:26:04 +0100

    Sull support for the IN* instructions

--- tests/increment_tests.cpp
+++ tests/increment_tests.cpp
@@ -161,6 +161,41 @@ TEST(IncrementTests, INCAbsoluteNoFlags)
 }
 
 // NOLINTNEXTLINE
+TEST(IncrementTests, INCAbsolutePluxXNoFlags)
+{
+    using TestData = std::pair<std::array<std::uint8_t, 3>, std::uint8_t>;
+
+    constexpr std::array<TestData, 4> programs{{
+        {{0xfe, 0x00, 0x02}, 0x01},
+        {{0xfe, 0x00, 0x03}, 0x02},
+        {{0xfe, 0x00, 0x04}, 0x03},
+        {{0xfe, 0x00, 0x05}, 0x7e},
+    }};
+
+    for (auto const& [program, init_v] : programs)
+    {
+        emulator::Cpu cpu;
+        // TODO : Set the memory to init
+        cpu.reg.x         = 0x0a;
+        std::uint16_t lsb = static_cast<std::uint16_t>(program[1]);
+        std::uint16_t hsb = static_cast<std::uint16_t>(program[2]);
+        std::uint16_t pos = (hsb << 8) | lsb + cpu.reg.x;
+        cpu.mem[pos]      = init_v;
+
+        emulator::execute(cpu, {program.data(), program.size()});
+
+        ASSERT_EQ(cpu.reg.a, 0x00);
+        ASSERT_EQ(cpu.reg.x, 0x0a);
+        ASSERT_EQ(cpu.reg.y, 0x00);
+        ASSERT_EQ(cpu.reg.sp, 0x00);
+        ASSERT_EQ(cpu.reg.pc, 0x03);
+
+        ASSERT_EQ(cpu.mem[pos], init_v + 1);
+        ASSERT_EQ(cpu.flags, make_flags(0b0000'0000));
+    }
+}
+
+// NOLINTNEXTLINE
 TEST(IncrementTests, INXZeroFlag)
 {
     constexpr std::array<std::uint8_t, 1> program = {0xe8};
@@ -230,6 +265,55 @@ TEST(IncrementTests, INCZeropagePluxXZeroFlag)
     ASSERT_EQ(cpu.reg.y, 0x00);
     ASSERT_EQ(cpu.reg.sp, 0x00);
     ASSERT_EQ(cpu.reg.pc, 0x02);
+
+    ASSERT_EQ(cpu.mem[0x0a], 0x00);
+    ASSERT_EQ(cpu.flags, make_flags(0b0000'0010));
+}
+
+// NOLINTNEXTLINE
+TEST(IncrementTests, INCAbsoluteZeroFlag)
+{
+    constexpr std::array<std::uint8_t, 3> program = {0xee, 0x00, 0x03};
+
+    emulator::Cpu cpu;
+
+    std::uint8_t const lsb  = program[1];
+    std::uint8_t const hsb  = program[2];
+    std::uint16_t const pos = ((hsb << 8) | lsb);
+
+    cpu.mem[pos] = 0xff;
+    emulator::execute(cpu, {program.data(), program.size()});
+
+    ASSERT_EQ(cpu.reg.a, 0x00);
+    ASSERT_EQ(cpu.reg.x, 0x00);
+    ASSERT_EQ(cpu.reg.y, 0x00);
+    ASSERT_EQ(cpu.reg.sp, 0x00);
+    ASSERT_EQ(cpu.reg.pc, 0x03);
+
+    ASSERT_EQ(cpu.mem[pos], 0x00);
+    ASSERT_EQ(cpu.flags, make_flags(0b0000'0010));
+}
+
+// NOLINTNEXTLINE
+TEST(IncrementTests, INCAbsolutePluxXZeroFlag)
+{
+    constexpr std::array<std::uint8_t, 3> program = {0xfe, 0x00, 0x03};
+
+    emulator::Cpu cpu;
+
+    cpu.reg.x               = 0x0a;
+    std::uint8_t const lsb  = program[1];
+    std::uint8_t const hsb  = program[2];
+    std::uint16_t const pos = ((hsb << 8) | lsb) + cpu.reg.x;
+
+    cpu.mem[pos] = 0xff;
+    emulator::execute(cpu, {program.data(), program.size()});
+
+    ASSERT_EQ(cpu.reg.a, 0x00);
+    ASSERT_EQ(cpu.reg.x, 0x0a);
+    ASSERT_EQ(cpu.reg.y, 0x00);
+    ASSERT_EQ(cpu.reg.sp, 0x00);
+    ASSERT_EQ(cpu.reg.pc, 0x03);
 
     ASSERT_EQ(cpu.mem[0x0a], 0x00);
     ASSERT_EQ(cpu.flags, make_flags(0b0000'0010));
@@ -374,10 +458,86 @@ TEST(IncrementTests, INCZeropagePlusXNegativeFlag)
     }
 }
 
-// TODO : Create tests for:
-//   - INC zeropage,X
-//   - INC absolute
-//   - INC absolute,X
+// NOLINTNEXTLINE
+TEST(IncrementTests, INCAbsoluteNegativeFlag)
+{
+    using TestData = std::pair<std::array<std::uint8_t, 3>, std::uint8_t>;
+
+    // clang-format off
+    constexpr std::array<TestData, 8> programs{{
+        {{0xee, 0x00, 0x02}, 0xfe},
+        {{0xee, 0x00, 0x03}, 0xef},
+        {{0xee, 0x00, 0x04}, 0xdf},
+        {{0xee, 0x10, 0x05}, 0xcf},
+        {{0xee, 0x50, 0x06}, 0xbf},
+        {{0xee, 0x80, 0x07}, 0xaf},
+        {{0xee, 0xe0, 0x08}, 0x9f},
+        {{0xee, 0xf0, 0x09}, 0x7f}
+    }};
+    // clang-format on
+
+    for (auto const& [program, init_v] : programs)
+    {
+        emulator::Cpu cpu;
+
+        std::uint8_t const lsb  = program[1];
+        std::uint8_t const hsb  = program[2];
+        std::uint16_t const pos = (hsb << 8) | lsb;
+        cpu.mem[pos]            = init_v;
+
+        emulator::execute(cpu, {program.data(), program.size()});
+
+        ASSERT_EQ(cpu.reg.a, 0x00);
+        ASSERT_EQ(cpu.reg.x, 0x00);
+        ASSERT_EQ(cpu.reg.y, 0x00);
+        ASSERT_EQ(cpu.reg.sp, 0x00);
+        ASSERT_EQ(cpu.reg.pc, 0x03);
+
+        ASSERT_EQ(cpu.mem[pos], init_v + 1);
+        ASSERT_EQ(cpu.flags, make_flags(0b1000'0000));
+    }
+}
+
+// NOLINTNEXTLINE
+TEST(IncrementTests, INCAbsolutePluxXNegativeFlag)
+{
+    using TestData = std::pair<std::array<std::uint8_t, 3>, std::uint8_t>;
+
+    // clang-format off
+    constexpr std::array<TestData, 8> programs{{
+        {{0xfe, 0x00, 0x02}, 0xfe},
+        {{0xfe, 0x00, 0x03}, 0xef},
+        {{0xfe, 0x00, 0x04}, 0xdf},
+        {{0xfe, 0x10, 0x05}, 0xcf},
+        {{0xfe, 0x50, 0x06}, 0xbf},
+        {{0xfe, 0x80, 0x07}, 0xaf},
+        {{0xfe, 0xe0, 0x08}, 0x9f},
+        {{0xfe, 0xf0, 0x09}, 0x7f}
+    }};
+    // clang-format on
+
+    for (auto const& [program, init_v] : programs)
+    {
+        emulator::Cpu cpu;
+
+        cpu.reg.x               = 0x0a;
+        std::uint8_t const lsb  = program[1];
+        std::uint8_t const hsb  = program[2];
+        std::uint16_t const pos = (hsb << 8) | lsb + cpu.reg.x;
+        cpu.mem[pos]            = init_v;
+
+        emulator::execute(cpu, {program.data(), program.size()});
+
+        ASSERT_EQ(cpu.reg.a, 0x00);
+        ASSERT_EQ(cpu.reg.x, 0x0a);
+        ASSERT_EQ(cpu.reg.y, 0x00);
+        ASSERT_EQ(cpu.reg.sp, 0x00);
+        ASSERT_EQ(cpu.reg.pc, 0x03);
+
+        ASSERT_EQ(cpu.mem[pos], init_v + 1);
+        ASSERT_EQ(cpu.flags, make_flags(0b1000'0000));
+    }
+}
 
 int main(int argc, char** argv)
 {
