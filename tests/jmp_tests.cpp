8e33be382139f215a35e955cd10bdde4a136c7f1
matheusgomes28 <matheusgarcia28@gmail.com>
2024-10-29 20:12:21 +0000

    Add JMP indirect tests

--- tests/jmp_tests.cpp
+++ tests/jmp_tests.cpp
@@ -32,3 +32,28 @@ TEST(JMPTests, AbsoluteTests)
     EXPECT_EQ(emulator::execute(cpu, program), 8);
     EXPECT_EQ(cpu.reg.pc, 0xffff);
 }
+
+// NOLINTNEXTLINE
+TEST(JMPTests, IndirectTests)
+{
+    std::array<std::uint8_t, 0xffff> program{};
+
+    emulator::Cpu cpu;
+
+    // 0x0000 -> 0x5554 (3 cycles)
+    program[0x0000] = 0x6c;
+    program[0x0001] = 0x54;
+    program[0x0002] = 0x55;
+
+    // 0x5554 -> 0xfffe (3 cycles)
+    program[0x5554] = 0xfe;
+    program[0x5555] = 0xff;
+
+    // noop to end the program
+    // 0xfffe noop (2 cycles)
+    program[0xfffe] = 0xea;
+
+
+    EXPECT_EQ(emulator::execute(cpu, program), 8);
+    EXPECT_EQ(cpu.reg.pc, 0xffff);
+}
